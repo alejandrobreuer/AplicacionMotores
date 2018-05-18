48e854b5b12ac43cacddee59292b815265e54b53
ga-abreuer <34494951+ga-abreuer@users.noreply.github.com>
2018-05-18 00:24:39 -0300

    Save Functionality

---
+++ QuestDesigner/Assets/Scripts/QuestDesigner/SaveMethod/QuestSaveManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+public class QuestSaveManager : MonoBehaviour {
+
+
+	public static void CreateAsset<T>() where T : ScriptableObject{
+
+		T asset = ScriptableObject.CreateInstance<T> ();
+
+		string path = AssetDatabase.GetAssetPath (Selection.activeObject);
+		if (path == "") {
+			path = "Assets";
+		} else if (Path.GetExtension (path) != ""){
+			path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)),"");
+		}
+
+		string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath (path + "/new " + typeof(T).ToString () + "");
+
+		AssetDatabase.CreateAsset (asset, assetPathAndName);
+
+		AssetDatabase.SaveAssets ();
+		AssetDatabase.Refresh ();
+		EditorUtility.FocusProjectWindow ();
+		Selection.activeObject = asset;
+
+
+
+	}
+
+
+}
