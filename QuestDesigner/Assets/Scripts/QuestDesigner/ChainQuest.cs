48365d3e051d9a812d5f1e5b93d3b8b7b9254428
ga-abreuer <34494951+ga-abreuer@users.noreply.github.com>
2018-05-29 20:49:41 -0300

    ChainQuest update
    Added Chain attributes as a node list in order to keep the quest line, and node position. (Updated by force to unity 2018 version, go back to 20173 plz)

--- QuestDesigner/Assets/Scripts/QuestDesigner/ChainQuest.cs
+++ QuestDesigner/Assets/Scripts/QuestDesigner/ChainQuest.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Experimental.UIElements.GraphView;
 
-public class ChainQuest : MonoBehaviour {
+public class ChainQuest {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public int chainQuestID;
+	public new string name;
+	public string description;
+	public QuestOrign originator;
+	public List<QuestRequirement> requirements;
+
+	public List<Node> chainQuestList;
+	public List<QuestReward> rewards;
+
+
+
 }
