2dcbe51755b15f9a5ffac677c1f4e5aee1f9e5f3
emiliano <38270665+EmilianoGaeta@users.noreply.github.com>
2018-05-30 17:45:11 -0300

    Add files via upload

---
+++ QuestDesigner/Assets/Editor/QuestDesigner/NodeQuest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeQuest
+{
+    public Rect myRect;
+    public string nodeName;
+    public float ID;
+    private bool _overNode;
+    public List<NodeQuest> connected;
+
+    public NodeQuest(float x, float y, float width, float height, string name)
+    {
+        myRect = new Rect(x, y, width, height);
+        connected = new List<NodeQuest>();
+        nodeName = name;
+    }
+
+    public void CheckMouse(Event cE, Vector2 pan)
+    {
+        if (myRect.Contains(cE.mousePosition - pan))
+            _overNode = true;
+        else
+            _overNode = false;
+    }
+
+    public bool OverNode
+    { get { return _overNode; } }
+}
